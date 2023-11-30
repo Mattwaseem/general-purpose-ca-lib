f914192efe045bb5740aca80019d4e421fb4ac31
Sahil-Patil-23 <140925512+Sahil-Patil-23@users.noreply.github.com>
2023-11-30 14:23:34 -0800

    Added skeleton for Von Neumann

--- src/von_neumannCA.cpp
+++ src/von_neumannCA.cpp
@@ -1,4 +1,42 @@
 #include <iostream>
 #include <vector>
-#include "cellular_automaton.h"
+#include "CellularAutomata.h"
 using namespace std;
+
+class VonNeumannCA : public CellularAutomata{
+    public:
+        VonNeumannCA(int width , int height) : CellularAutomata(width , height){}
+
+        void initializeGrid() override{
+            // Implementation for initializing Von Neumann Grid
+            // We will set initial states of each cell here
+        }
+
+        void updateGrid() override
+        {
+            std::vector<std::vector<int>> newGrid = grid;
+
+            for (int y = 0; y < height; ++y)
+            {
+                for (int x = 0; x < width; ++x)
+                {
+                    // Get the state of the current cell and its neighbors
+                    int currentState = grid[x][y];
+                    int northNeighbor = (y > 0) ? grid[x][y - 1] : 0;
+                    int southNeighbor = (y < height - 1) ? grid[x][y + 1] : 0;
+                    int eastNeighbor = (x < width - 1) ? grid[x + 1][y] : 0;
+                    int westNeighbor = (x > 0) ? grid[x - 1][y] : 0;
+
+                    // Implement your Von Neumann CA rules here
+                    // Update the newGrid based on the current state and neighbors
+                    // newGrid[x][y] = updatedState;
+
+                    
+                }
+            }
+
+            // Update the grid with the new state
+            grid = newGrid;
+        }
+
+};
