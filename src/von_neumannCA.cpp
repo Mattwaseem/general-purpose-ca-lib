b41477f77ee924efda8330cbca123c94eaef94a5
Sahil-Patil-23 <140925512+Sahil-Patil-23@users.noreply.github.com>
2023-12-04 21:49:15 -0800

    Made some updates to how the Von Neumann class is structured/written

--- src/von_neumannCA.cpp
+++ src/von_neumannCA.cpp
@@ -10,65 +10,105 @@ using namespace std;
             // We will set initial states of each cell here
         }
 
-        void VonNeumannCA::updateGrid()
-class VonNeumannCA : public CellularAutomata
-{
-public:
-    VonNeumannCA(int width, int height) : CellularAutomata(width, height) {}
+        void VonNeumannCA::updateGrid() {
+            vector<vector<int>> newgrid = grid;
 
-    void initializeGrid() override
-    {
-        // Implementation for initializing Von Neumann Grid
-        // We will set initial states of each cell here
-    }
+            for(int y=0 ; y<height ; ++y){
+                for(int x=0 ; x<width ; ++x){
+                    for(int x=0 ; x<width ; ++x){
+                        // Retrieving the state of the current cell at coordinates (x,y)
+                        int currentState = grid[x][y];
 
-    void updateGrid() override
-    {
-        std::vector<std::vector<int>> newGrid = grid;
+                        // Checking to see if the north neighbor isn't top edge of grid- if it is, it defaults to state 0
+                        int northNeighbor = (y > 0) ? grid[x][y - 1] : 0;
 
-        for (int y = 0; y < height; ++y)
-        {
-            for (int x = 0; x < width; ++x)
-            {
-                for (int x = 0; x < width; ++x)
-                {
-                    // Get the state of the current cell and its neighbors
+                        // Checking to see if the south neighbor isn't top edge of grid- if it is, it defaults to state 0
+                        int southNeighbor = (y < height - 1) ? grid[x][y + 1] : 0;
 
-                    // Retrieving the state of the current cell at coordinates (x,y)
+                        // Checking to see if the east neighbor isn't top edge of grid- if it is, it defaults to state 0
+                        int eastNeighbor = (x < width - 1) ? grid[x + 1][y] : 0;
+
+                        //  Checking to see if the west neighbor isn't top edge of grid- if it is, it defaults to state 0
+                        int westNeighbor = (x > 0) ? grid[x - 1][y] : 0;
+
+                        // Implement your Von Neumann CA rules here
+                        // Update the newGrid based on the current state and neighbors
+                        // newgrid[x][y] = updatedState;
+
+
+                    }
+//                 // Get the state of the current cell and its neighbors
                     int currentState = grid[x][y];
-
-                    // Checking to see if the north neighbor isn't top edge of grid- if it is, it defaults to state 0
                     int northNeighbor = (y > 0) ? grid[x][y - 1] : 0;
-
-                    // Checking to see if the south neighbor isn't top edge of grid- if it is, it defaults to state 0
                     int southNeighbor = (y < height - 1) ? grid[x][y + 1] : 0;
-
-                    // Checking to see if the east neighbor isn't top edge of grid- if it is, it defaults to state 0
                     int eastNeighbor = (x < width - 1) ? grid[x + 1][y] : 0;
-
-                    //  Checking to see if the west neighbor isn't top edge of grid- if it is, it defaults to state 0
                     int westNeighbor = (x > 0) ? grid[x - 1][y] : 0;
 
-                    // Implement your Von Neumann CA rules here
-                    // Update the newGrid based on the current state and neighbors
-                    // newGrid[x][y] = updatedState;
-
-
+//                 // Implement your Von Neumann CA rules here
+//                 // Update the newGrid based on the current state and neighbors
+//                 // newGrid[x][y] = updatedState;
+                    }
                 }
-                // Get the state of the current cell and its neighbors
-                int currentState = grid[x][y];
-                int northNeighbor = (y > 0) ? grid[x][y - 1] : 0;
-                int southNeighbor = (y < height - 1) ? grid[x][y + 1] : 0;
-                int eastNeighbor = (x < width - 1) ? grid[x + 1][y] : 0;
-                int westNeighbor = (x > 0) ? grid[x - 1][y] : 0;
-
-                // Implement your Von Neumann CA rules here
-                // Update the newGrid based on the current state and neighbors
-                // newGrid[x][y] = updatedState;
             }
-        }
+        
+// class VonNeumannCA : public CellularAutomata
+// {
+// public:
+//     VonNeumannCA(int width, int height) : CellularAutomata(width, height) {}
 
-        void CellularAutomate::displayGrid() const{}
+//     void initializeGrid() override
+//     {
+//         // Implementation for initializing Von Neumann Grid
+//         // We will set initial states of each cell here
+//     }
+
+//     void updateGrid() override
+//     {
+//         std::vector<std::vector<int>> newGrid = grid;
+
+//         for (int y = 0; y < height; ++y)
+//         {
+//             for (int x = 0; x < width; ++x)
+//             {
+//                 for (int x = 0; x < width; ++x)
+//                 {
+//                     // Get the state of the current cell and its neighbors
+
+//                     // Retrieving the state of the current cell at coordinates (x,y)
+//                     int currentState = grid[x][y];
+
+//                     // Checking to see if the north neighbor isn't top edge of grid- if it is, it defaults to state 0
+//                     int northNeighbor = (y > 0) ? grid[x][y - 1] : 0;
+
+//                     // Checking to see if the south neighbor isn't top edge of grid- if it is, it defaults to state 0
+//                     int southNeighbor = (y < height - 1) ? grid[x][y + 1] : 0;
+
+//                     // Checking to see if the east neighbor isn't top edge of grid- if it is, it defaults to state 0
+//                     int eastNeighbor = (x < width - 1) ? grid[x + 1][y] : 0;
+
+//                     //  Checking to see if the west neighbor isn't top edge of grid- if it is, it defaults to state 0
+//                     int westNeighbor = (x > 0) ? grid[x - 1][y] : 0;
+
+//                     // Implement your Von Neumann CA rules here
+//                     // Update the newGrid based on the current state and neighbors
+//                     // newGrid[x][y] = updatedState;
+
+
+//                 }
+//                 // Get the state of the current cell and its neighbors
+//                 int currentState = grid[x][y];
+//                 int northNeighbor = (y > 0) ? grid[x][y - 1] : 0;
+//                 int southNeighbor = (y < height - 1) ? grid[x][y + 1] : 0;
+//                 int eastNeighbor = (x < width - 1) ? grid[x + 1][y] : 0;
+//                 int westNeighbor = (x > 0) ? grid[x - 1][y] : 0;
+
+//                 // Implement your Von Neumann CA rules here
+//                 // Update the newGrid based on the current state and neighbors
+//                 // newGrid[x][y] = updatedState;
+//             }
+//         }
+
+//         void CellularAutomate::displayGrid() const{}
 
 
 
@@ -82,7 +122,4 @@ int main() {
 
     return 0;
 }
-        // Update the grid with the new state
-        grid = newGrid;
-    }
-};
+
