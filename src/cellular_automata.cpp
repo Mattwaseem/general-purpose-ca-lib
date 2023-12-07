e2edb3273aade711b8f870014fa03b6465e6b9b0
AishaTDR <aisha.thermidor@berkeley.edu>
2023-12-06 21:51:43 -0600

    12/06 Classwork

--- src/cellular_automata.cpp
+++ src/cellular_automata.cpp
@@ -3,6 +3,173 @@
 #include <functional>
 #include <random>
 
+enum class GridDimension { OneD, TwoD };
+enum class BoundaryCondition { Fixed, Periodic, NoBoundary };
+enum class NeighborhoodType { Moore, VonNeumann };
+
+class CellularAutomata {
+public:
+    using Grid1D = std::vector<int>;
+    using Grid2D = std::vector<std::vector<int>>;
+    using RuleFunction1D = std::function<int(int, int)>; // Function type for 1D
+    using RuleFunction2D = std::function<int(int, int)>; // Function type for 2D
+    using InitializationFunction1D = std::function<void(Grid1D&)>;
+    using InitializationFunction2D = std::function<void(Grid2D&)>;
+
+    CellularAutomata(int size, GridDimension dimension, BoundaryCondition bc, NeighborhoodType nt)
+        : size_(size), dimension_(dimension), boundary_condition_(bc), neighborhood_type_(nt) {
+        if (dimension == GridDimension::OneD) {
+            grid_1d_.resize(size, 0);
+        } else {
+            grid_2d_.resize(size, std::vector<int>(size, 0));
+        }
+    }
+
+    void Initialize1D(const InitializationFunction1D& init_func) {
+        if (dimension_ == GridDimension::OneD) {
+            init_func(grid_1d_);
+        } else {
+            throw std::runtime_error("Initialization function for 1D grid called on a non-1D automaton");
+        }
+    }
+
+    void Initialize2D(const InitializationFunction2D& init_func) {
+        if (dimension_ == GridDimension::TwoD) {
+            init_func(grid_2d_);
+        } else {
+            throw std::runtime_error("Initialization function for 2D grid called on a non-2D automaton");
+        }
+    }
+
+    void ApplyRule1D(const RuleFunction1D& rule_func) {
+        if (dimension_ == GridDimension::OneD) {
+            Grid1D new_grid = grid_1d_;
+            for (int i = 0; i < size_; ++i) {
+                int neighbors = CalculateNeighbors1D(i);
+                new_grid[i] = rule_func(neighbors, grid_1d_[i]);
+            }
+            grid_1d_ = new_grid;
+        } else {
+            throw std::runtime_error("Rule function for 1D grid called on a non-1D automaton");
+        }
+    }
+
+    void ApplyRule2D(const RuleFunction2D& rule_func) {
+        if (dimension_ == GridDimension::TwoD) {
+            Grid2D new_grid = grid_2d_;
+            for (int i = 0; i < size_; ++i) {
+                for (int j = 0; j < size_; ++j) {
+                    int neighbors = CalculateNeighbors2D(i, j);
+                    new_grid[i][j] = rule_func(neighbors, grid_2d_[i][j]);
+                }
+            }
+            grid_2d_ = new_grid;
+        } else {
+            throw std::runtime_error("Rule function for 2D grid called on a non-2D automaton");
+        }
+    }
+
+    void Print() const {
+        if (dimension_ == GridDimension::OneD) {
+            for (int cell : grid_1d_) {
+                std::cout << cell << " ";
+            }
+            std::cout << "\n";
+        } else {
+            for (const auto &row : grid_2d_) {
+                for (int cell : row) {
+                    std::cout << cell << " ";
+                }
+                std::cout << "\n";
+            }
+        }
+    }
+
+private:
+    int size_;
+    GridDimension dimension_;
+    BoundaryCondition boundary_condition_;
+    NeighborhoodType neighborhood_type_;
+    Grid1D grid_1d_;
+    Grid2D grid_2d_;
+
+    int CalculateNeighbors1D(int index) const {
+        int neighbors = 0;
+        for (int di = -1; di <= 1; ++di) {
+            if (di == 0) continue; // Skip the center cell
+            int ni = index + di;
+            // Boundary condition handling for 1D
+            if (boundary_condition_ == BoundaryCondition::Periodic) {
+                ni = (ni + size_) % size_;
+            } else if (boundary_condition_ == BoundaryCondition::NoBoundary) {
+                if (ni < 0 || ni >= size_) continue;
+            }
+            neighbors += grid_1d_[ni];
+        }
+        return neighbors;
+    }
+
+    int CalculateNeighbors2D(int i, int j) const {
+        int neighbors = 0;
+        for (int di = -1; di <= 1; ++di) {
+            for (int dj = -1; dj <= 1; ++dj) {
+                if (di == 0 && dj == 0) continue; // Skip the center cell
+                int ni = i + di;
+                int nj = j + dj;
+                // Boundary condition handling for 2D
+                if (boundary_condition_ == BoundaryCondition::Periodic) {
+                    ni = (ni + size_) % size_;
+                    nj = (nj + size_) % size_;
+                } else if (boundary_condition_ == BoundaryCondition::NoBoundary) {
+                    if (ni < 0 || ni >= size_ || nj < 0 || nj >= size_) continue;
+                }
+                neighbors += grid_2d_[ni][nj];
+            }
+        }
+        return neighbors;
+    }
+};
+
+int main() {
+    int size = 10;
+    CellularAutomata ca2D(size, GridDimension::TwoD, BoundaryCondition::Periodic, NeighborhoodType::Moore);
+
+    // Define the initialization function for 2D
+    CellularAutomata::InitializationFunction2D init_func_2d = [](CellularAutomata::Grid2D& grid) {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> distrib(0, 1);
+
+        for (auto &row : grid) {
+            for (int &cell : row) {
+                cell = distrib(gen);
+            }
+        }
+    };
+
+    // Define the rule function for 2D
+    CellularAutomata::RuleFunction2D rule_func_2d = [&ca2D](int neighbors, int current_state) {
+        // Example rule: A cell becomes alive (1) if exactly 3 neighbors, otherwise stays the same
+        return (neighbors == 3) ? 1 : current_state;
+    };
+
+    // Initialize and apply rules to the 2D cellular automaton
+    ca2D.Initialize2D(init_func_2d);
+    ca2D.ApplyRule2D(rule_func_2d);
+
+    // Print the final state of the grid
+    ca2D.Print();
+
+    return 0;
+}
+
+
+/*
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <random>
+
 class CellularAutomata3D {
 public:
     using Grid = std::vector<std::vector<std::vector<int>>>;
@@ -118,3 +285,4 @@ int main() {
 
     return 0;
 }
+*/
